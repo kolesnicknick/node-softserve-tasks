9c557c0e2b8f85a4a303f09ae539ba9f6decd415
kolesnicknick <kolesniknikolai92@gmail.com>
2020-02-26 00:14:52 +0530

    fixed lots of stuff

--- app.js
+++ app.js
@@ -4,46 +4,44 @@ const fs = require('fs');
 const uuid = require('uuid/v4');
 
 const server = http.createServer((req, res) => {
+    req.setEncoding('utf8');
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    }).on('end', () => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.setHeader('Content-Type', 'application/json');
+    
+        if (req.url === '/tasks') {
+            fs.readdir(path.join(__dirname, 'tasks'), (err, tasks) => {
+                if (err) { throw err; }
+                let responseBody = [];
+                tasks.forEach(task => {
+                    responseBody.push(JSON.parse(fs.readFileSync(path.join(__dirname, 'tasks', task), 'UTF-8')));
+                });
 
-    if (req.url === '/') {
-        console.log(req.method);
-    }
-
-    if (req.url === '/tasks') {
-        fs.readdir(path.join(__dirname, 'tasks'), (err, tasks) => {
-            if (err) { throw err; }
-            let responseBody = [];
-            tasks.forEach(task => {
-                responseBody.push(JSON.parse(fs.readFileSync(path.join(__dirname, 'tasks', task), 'UTF-8')));
+                res.end(JSON.stringify(responseBody), 'UTF-8');
+                return;
             });
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(responseBody), 'UTF-8');
-        });
-    }
-
-    if (req.url === '/tasks' && req.method === 'POST') {
-        let body = '';
-            req.on('data', chunk => {
-                body += chunk.toString(); // convert Buffer to string
+        }
+    
+        if (req.url === '/tasks' && req.method === 'POST') {
+            let data = new Uint8Array(Buffer.from(body));
+            console.log(`${req.url} ${req.method} BODY: ${body}`)
+    
+            fs.writeFile(path.join(__dirname, 'tasks', `${uuid().split('-')[0]}.json`), data, (err) => {
+                if (err) { throw err; }
+                res.end(`New task created `);
+                return;
             });
-            req.on('end', () => {
-                console.log(body);
-                res.end('ok');
-            });
-        
-        fs.writeFile(path.join(__dirname, 'tasks', `${uuid}.json`), body, (err) => {
-            if (err) { throw err; }
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(`New task created `);
-        });
-    }
+        }
+    });
 
-
-    if (req.url === '/tasks/:id' && req.method === 'GET') {
-        //return task by id
-    }
+    
 })
-
 
 const PORT = process.env.port || 4724
 server.listen(PORT, () => console.log('Running server on port ' + PORT));
